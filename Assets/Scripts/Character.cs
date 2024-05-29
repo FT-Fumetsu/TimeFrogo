b0fca47cf74b0714931c5a09aa85009c92367e7e
FT-Fumetsu <esmieudamien18@gmail.com>
2024-05-29 15:09:06 +0200

    add playerMove script

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,43 +10,19 @@ using UnityEngine.UI;
 public class Character : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private Character _player;
-    [SerializeField] private Transform _transform;
-    [SerializeField] private GroundSpawn _groundGenerator;
-    [SerializeField] private FollowPlayer _followPlayer;
     [SerializeField] private GroundSpawn _groundSpawn;
-    [SerializeField] private TextMeshProUGUI _textScore;
     [SerializeField] private Echo _echo;
-    [SerializeField] private RotationPlayer _visuPlayer;
-    [SerializeField] private Vehicle _vehicle;
-    [SerializeField] private Chrono _timer;
     [SerializeField] private RestartQuit _buttonMenu;
-    [SerializeField] private FailedPauseMenu _faildedPauseMenu;
+    [SerializeField] private FailedPauseMenu _failedPauseMenu;
     [SerializeField] private AudioManager _audioManager;
+    [SerializeField] private PlayerMove _playerMove;
 
     [Header("Balancing")]
-    [SerializeField,Range(0,10)] private float _inputTimer;
-    [SerializeField, Range(0, 10)] private float _getKeyTimer;
-    [SerializeField] private float _movement = 1.0f;
-    [SerializeField] private float _maxRightPosition = 3.0f;
-    [SerializeField] private float _maxLeftPosition = 3.0f;
-    [SerializeField] private KeyCode _leftKey = KeyCode.LeftArrow;
-    [SerializeField] private KeyCode _rightKey = KeyCode.RightArrow;
-    [SerializeField] private KeyCode _upKey = KeyCode.UpArrow;
-    [SerializeField] private KeyCode _downKey = KeyCode.DownArrow;
     [SerializeField] private KeyCode _escape = KeyCode.Escape;
-    [SerializeField] private float _speed;
-
     [SerializeField] private Transform _graphics = null;
 
     private float _playerSpeed;
     public bool _paused;
-    float _score = 0;
-    private float _chrono;
-    public float _maxPositionZReach;
-    public float _currentPositionX = 0.0f;
-    public float _currentPositionZ = 0.0f;
-    private float xpos;
     private Collider _lastIcePlatform = null;
     private GameObject _currentIcePlatform = null;
 
@@ -63,91 +39,19 @@ public class Character : MonoBehaviour
     private void Update()
     {
         transform.Translate(Vector3.right * _playerSpeed * Time.deltaTime);
-        _chrono += Time.deltaTime;
-        RaycastHit hit;
         DrawRaycasts();
 
         if (_paused == true)
         {
-            _faildedPauseMenu.PauseMenu();
+            _failedPauseMenu.PauseMenu();
         }
 
         if (_paused == false)
         {
-            _faildedPauseMenu.Unpause();
+            _failedPauseMenu.Unpause();
         }
         
-        if (Input.GetKeyDown(_leftKey) && _chrono > _inputTimer)
-        {
-            _chrono = 0;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + .45f), transform.TransformDirection(Vector3.left), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - .45f), transform.TransformDirection(Vector3.left), out hit, 1))
-            {
-                if (hit.transform.tag == "Obstacles")
-                {
-                    return;
-                }
-            }
-            else
-            {
-                MoveLeft();
-            }
-        }
-
-        if (Input.GetKeyDown(_rightKey) && _chrono > _inputTimer)
-        {
-            _chrono = 0;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + .45f), transform.TransformDirection(Vector3.right), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - .45f), transform.TransformDirection(Vector3.right), out hit, 1))
-            {
-                if (hit.transform.tag == "Obstacles")
-                {
-                    Debug.Log(hit.transform.name + " right");
-                }
-            }
-            else
-            {
-                MoveRight();
-            }
-        }
-
-        if (Input.GetKeyDown(_upKey) && _chrono > _inputTimer)
-        {
-            _chrono = 0;
-            if (Physics.Raycast(new Vector3(transform.position.x + 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x - 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 1) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1))
-            {
-                if (hit.transform.tag == "Obstacles")
-                {
-                    Debug.Log(hit.transform.name + "up");
-                }                        
-            }
-            else
-            {
-                MoveUp();
-                if (_currentPositionZ <= _maxPositionZReach)
-                {
-                    return;
-                }
-                else
-                {
-                    SpawnGround();
-                }
-            }
-        }
-
-        if (Input.GetKeyDown(_downKey) && _chrono > _inputTimer)
-        {
-            _chrono = 0;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x + 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.back), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x - 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.back), out hit, 1))
-            {
-                if (hit.transform.tag == "Obstacles")
-                {
-                    return;
-                }
-            }
-            else
-            {
-                MoveDown();
-            }
-        }
+        
 
         if (Input.GetKeyDown(_escape) && _paused == false)
         {
@@ -160,80 +64,6 @@ public class Character : MonoBehaviour
             _paused = false;
             Time.timeScale = 1;
         }
-
-        if (Input.GetKey(_leftKey) && _chrono > _getKeyTimer)
-        {
-            _chrono = 0;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + .45f), transform.TransformDirection(Vector3.left), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - .45f), transform.TransformDirection(Vector3.left), out hit, 1))
-            {
-                if (hit.transform.tag == "Obstacles")
-                {
-                    return;
-                }
-            }
-            else
-            {
-                MoveLeft();
-            }
-        }
-
-        if (Input.GetKey(_rightKey) && _chrono > _getKeyTimer)
-        {
-            _chrono = 0;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + .45f), transform.TransformDirection(Vector3.right), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - .45f), transform.TransformDirection(Vector3.right), out hit, 1))
-            {
-                if (hit.transform.tag == "Obstacles")
-                {
-                    Debug.Log(hit.transform.name + " right");
-                }
-            }
-            else
-            {
-                MoveRight();
-            }
-        }
-
-        if (Input.GetKey(_upKey) && _chrono > _getKeyTimer)
-        {
-            _chrono = 0;
-            if (Physics.Raycast(new Vector3(transform.position.x + 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x - 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 1) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1))
-            {
-                if (hit.transform.tag == "Obstacles")
-                {
-                    Debug.Log(hit.transform.name + "up");
-                }
-            }
-            else
-            {
-                _player.MoveUp();
-                if (_player._currentPositionZ <= _player._maxPositionZReach)
-                {
-                    return;
-                }
-                else
-                {
-                    SpawnGround();
-                }
-            }
-        }
-
-        if (Input.GetKey(_downKey) && _chrono > _getKeyTimer)
-        {
-            _chrono = 0;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x + 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.back), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x - 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.back), out hit, 1))
-            {
-                if (hit.transform.tag == "Obstacles")
-                {
-                    return;
-                }
-            }
-            else
-            {
-                MoveDown();
-            }
-        }
-
-        _textScore.SetText("Score : " + _score.ToString());
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -242,14 +72,14 @@ public class Character : MonoBehaviour
             GameOver();
             _audioManager.PlaySFX(_audioManager._deathCrush);
             _audioManager.PlaySFX(_audioManager._deathTruck);
-            _groundGenerator._isAlive = false;
+            _groundSpawn._isAlive = false;
             DestroyPlayer();
         }
         else if(other.gameObject.tag == "Water")
         {
             GameOver();
             _audioManager.PlaySFX(_audioManager._splashSFX);
-            _groundGenerator._isAlive = false;
+            _groundSpawn._isAlive = false;
             DestroyPlayer();
         }
         else if (other.gameObject.tag == "Crate")
@@ -257,26 +87,26 @@ public class Character : MonoBehaviour
             GameOver();
             _audioManager.PlaySFX(_audioManager._deathCrush);
             _audioManager.PlaySFX(_audioManager._deathTruck);
-            _groundGenerator._isAlive = false;
+            _groundSpawn._isAlive = false;
             DestroyPlayer();
         }
         else if(other.gameObject.tag == "Echo")
         {
             GameOver();
-            _groundGenerator._isAlive = false;
+            _groundSpawn._isAlive = false;
             Destroy(gameObject);
         }
         else if(other.gameObject.tag == "Trapdoor")
         {
             GameOver();
             _audioManager.PlaySFX(_audioManager._fall);
-            _groundGenerator._isAlive = false;
+            _groundSpawn._isAlive = false;
             DestroyPlayer();
         }
         else if (other.gameObject.tag == "CrackedIce" || other.gameObject.tag == "Laser")
         {
-            _faildedPauseMenu.GameOver();
-            _groundGenerator._isAlive = false;
+            GameOver();
+            _groundSpawn._isAlive = false;
             Destroy(gameObject);
         }
     }
@@ -289,7 +119,7 @@ public class Character : MonoBehaviour
             int roundXPos = Mathf.RoundToInt(xPos);
             Vector3 pos = new Vector3(roundXPos, transform.position.y, transform.position.z);
             transform.position = pos;
-            _currentPositionX = roundXPos;
+            _playerMove._currentPositionX = roundXPos;
 
             _echo.AddExitPlatformPosition(pos);
             _echo.InvokeExitPlatform();
@@ -313,99 +143,8 @@ public class Character : MonoBehaviour
             }            
         }
     }
-    public void MoveRight()
-    {
-        if (_currentPositionX >= _maxRightPosition)
-        {
-            return;
-        }
+  
 
-        if (_lastIcePlatform != null)
-        {
-            _currentPositionX = Mathf.RoundToInt(transform.position.x);
-        }
-        _audioManager.PlaySFX(_audioManager._jump);
-        _visuPlayer.RotationRight();
-        _currentPositionX += _movement;
-        ApplyXPosition();
-        _echo.InvokeEchoMoveRight(transform.position);
-        if(_lastIcePlatform != null)
-        {
-            _lastIcePlatform.enabled = false;
-        }
-        transform.SetParent(null);
-        _echo.InvokeEchoStopSlide(transform.position);
-    }
-    public void MoveLeft()
-    {
-        if (_currentPositionX <= _maxLeftPosition)
-        {
-            return;
-        }
-
-        if(_lastIcePlatform != null)
-        {
-            _currentPositionX = Mathf.RoundToInt(transform.position.x);
-        }
-
-        _audioManager.PlaySFX(_audioManager._jump);
-        _visuPlayer.RotationLeft();
-        _currentPositionX -= _movement;
-        ApplyXPosition();
-        _echo.InvokeEchoMoveLeft(transform.position);
-        if(_lastIcePlatform != null)
-        {
-            _lastIcePlatform.enabled = false;
-        }
-        transform.SetParent(null);
-        //_playerSpeed = 0;
-        _echo.InvokeEchoStopSlide(transform.position);
-    }
-    public void MoveUp()
-    {
-        _audioManager.PlaySFX(_audioManager._jump);
-        _visuPlayer.RotationUp();
-        _currentPositionZ += _movement;
-        ApplyZPosition();
-        _echo.InvokeEchoMoveUp(transform.position);
-        transform.SetParent(null);
-        //_playerSpeed = 0;
-        _echo.InvokeEchoStopSlide(transform.position);
-    }
-    public void MoveDown()
-    {
-        _audioManager.PlaySFX(_audioManager._jump);
-        _visuPlayer.RotationDown();
-        _currentPositionZ -= _movement;
-        ApplyZPosition();
-        _echo.InvokeEchoMoveDown(transform.position);
-        transform.SetParent(null);
-        //_playerSpeed = 0;
-        _echo.InvokeEchoStopSlide(transform.position);
-    }
-    private void ApplyXPosition()
-    {
-        Vector3 newPosition = transform.position;
-        newPosition.x = _currentPositionX;
-        _transform.position = newPosition;
-
-        PlayMovementTween(newPosition);
-    }
-    private void ApplyZPosition()
-    {
-        Vector3 newPosition = transform.position;
-        newPosition.z = _currentPositionZ;
-        _transform.position = newPosition;
-
-        PlayMovementTween(newPosition);
-    }
-    public void SpawnGround()
-    {
-        _followPlayer.MoveUp();
-        _maxPositionZReach = _currentPositionZ;
-        _score++;
-        _groundSpawn.SpawnGround(transform.position);
-    }
     private void DrawRaycasts()
     {
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1, Color.red);
@@ -430,15 +169,14 @@ public class Character : MonoBehaviour
     }
     private void GameOver()
     {
-        _faildedPauseMenu._failedMenu.SetActive(true);
-        _groundGenerator._truckSfx.mute = true;
-        _groundGenerator._forestSfx.mute = true;
-        _groundGenerator._futurAmbianceSfx.mute = true;
-        _groundGenerator._snowStormSfx.mute = true;
-        _groundGenerator._riverSfx.mute = true;
+        _failedPauseMenu._failedMenu.SetActive(true);
+        _groundSpawn._truckSfx.mute = true;
+        _groundSpawn._forestSfx.mute = true;
+        _groundSpawn._futurAmbianceSfx.mute = true;
+        _groundSpawn._snowStormSfx.mute = true;
+        _groundSpawn._riverSfx.mute = true;
     }
-
-    private void PlayMovementTween(Vector3 newPosition)
+    public void PlayMovementTween(Vector3 newPosition)
     {
         _movementTween?.Kill();
         _graphics.DOMove(newPosition, .1f).SetEase(Ease.InOutBack);
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,278 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Referencences")]
+    [SerializeField] private Character _player;
+    [SerializeField] private Echo _echo;
+    [SerializeField] private AudioManager _audioManager;
+    [SerializeField] private RotationPlayer _visuPlayer;
+    [SerializeField] private Transform _transform;
+    [SerializeField] private FollowPlayer _followPlayer;
+    [SerializeField] private GroundSpawn _groundSpawn;
+    [SerializeField] private TextMeshProUGUI _textScore;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Balancing")]
+    [SerializeField, Range(0, 10)] private float _inputTimer;
+    [SerializeField, Range(0, 10)] private float _getKeyTimer;
+    [SerializeField] private KeyCode _leftKey = KeyCode.LeftArrow;
+    [SerializeField] private KeyCode _rightKey = KeyCode.RightArrow;
+    [SerializeField] private KeyCode _upKey = KeyCode.UpArrow;
+    [SerializeField] private KeyCode _downKey = KeyCode.DownArrow;
+    [SerializeField] private float _movement = 1.0f;
+    [SerializeField] private float _maxRightPosition = 3.0f;
+    [SerializeField] private float _maxLeftPosition = -3.0f;
+
+    private float _chrono;
+    public float _maxPositionZReach;
+    public float _currentPositionX = 0.0f;
+    public float _currentPositionZ = 0.0f;
+    float _score = 0;
+    private Collider _lastIcePlatform = null;
+    private void Update()
     {
-        
+        _chrono += Time.deltaTime;
+        RaycastHit hit;
+        if (Input.GetKeyDown(_leftKey) && _chrono > _inputTimer)
+        {
+            _chrono = 0;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + .45f), transform.TransformDirection(Vector3.left), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - .45f), transform.TransformDirection(Vector3.left), out hit, 1))
+            {
+                if (hit.transform.tag == "Obstacles")
+                {
+                    return;
+                }
+            }
+            else
+            {
+                MoveLeft();
+            }
+        }
+
+        if (Input.GetKeyDown(_rightKey) && _chrono > _inputTimer)
+        {
+            _chrono = 0;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + .45f), transform.TransformDirection(Vector3.right), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - .45f), transform.TransformDirection(Vector3.right), out hit, 1))
+            {
+                if (hit.transform.tag == "Obstacles")
+                {
+                    Debug.Log(hit.transform.name + " right");
+                }
+            }
+            else
+            {
+                MoveRight();
+            }
+        }
+
+        if (Input.GetKeyDown(_upKey) && _chrono > _inputTimer)
+        {
+            _chrono = 0;
+            if (Physics.Raycast(new Vector3(transform.position.x + 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x - 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 1) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1))
+            {
+                if (hit.transform.tag == "Obstacles")
+                {
+                    Debug.Log(hit.transform.name + "up");
+                }
+            }
+            else
+            {
+                MoveUp();
+                if (_currentPositionZ <= _maxPositionZReach)
+                {
+                    return;
+                }
+                else
+                {
+                    SpawnGround();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(_downKey) && _chrono > _inputTimer)
+        {
+            _chrono = 0;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x + 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.back), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x - 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.back), out hit, 1))
+            {
+                if (hit.transform.tag == "Obstacles")
+                {
+                    return;
+                }
+            }
+            else
+            {
+                MoveDown();
+            }
+        }
+        if (Input.GetKey(_leftKey) && _chrono > _getKeyTimer)
+        {
+            _chrono = 0;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + .45f), transform.TransformDirection(Vector3.left), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - .45f), transform.TransformDirection(Vector3.left), out hit, 1))
+            {
+                if (hit.transform.tag == "Obstacles")
+                {
+                    return;
+                }
+            }
+            else
+            {
+                MoveLeft();
+            }
+        }
+
+        if (Input.GetKey(_rightKey) && _chrono > _getKeyTimer)
+        {
+            _chrono = 0;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + .45f), transform.TransformDirection(Vector3.right), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z - .45f), transform.TransformDirection(Vector3.right), out hit, 1))
+            {
+                if (hit.transform.tag == "Obstacles")
+                {
+                    Debug.Log(hit.transform.name + " right");
+                }
+            }
+            else
+            {
+                MoveRight();
+            }
+        }
+
+        if (Input.GetKey(_upKey) && _chrono > _getKeyTimer)
+        {
+            _chrono = 0;
+            if (Physics.Raycast(new Vector3(transform.position.x + 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x - 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 1) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1))
+            {
+                if (hit.transform.tag == "Obstacles")
+                {
+                    Debug.Log(hit.transform.name + "up");
+                }
+            }
+            else
+            {
+                MoveUp();
+                if (_currentPositionZ <= _maxPositionZReach)
+                {
+                    return;
+                }
+                else
+                {
+                    SpawnGround();
+                }
+            }
+        }
+
+        if (Input.GetKey(_downKey) && _chrono > _getKeyTimer)
+        {
+            _chrono = 0;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x + 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.back), out hit, 1) || Physics.Raycast(new Vector3(transform.position.x - 0.45f, transform.position.y, transform.position.z), transform.TransformDirection(Vector3.back), out hit, 1))
+            {
+                if (hit.transform.tag == "Obstacles")
+                {
+                    return;
+                }
+            }
+            else
+            {
+                MoveDown();
+            }
+        }
+        _textScore.SetText("Score : " + _score.ToString());
+    }
+    public void MoveRight()
+    {
+        if (_currentPositionX >= _maxRightPosition)
+        {
+            return;
+        }
+
+        if (_lastIcePlatform != null)
+        {
+            _currentPositionX = Mathf.RoundToInt(transform.position.x);
+        }
+        _audioManager.PlaySFX(_audioManager._jump);
+        _visuPlayer.RotationRight();
+        _currentPositionX += _movement;
+        ApplyXPosition();
+        _echo.InvokeEchoMoveRight(transform.position);
+        if (_lastIcePlatform != null)
+        {
+            _lastIcePlatform.enabled = false;
+        }
+        transform.SetParent(null);
+        _echo.InvokeEchoStopSlide(transform.position);
+    }
+    public void MoveLeft()
+    {
+        if (_currentPositionX <= _maxLeftPosition)
+        {
+            return;
+        }
+
+        if (_lastIcePlatform != null)
+        {
+            _currentPositionX = Mathf.RoundToInt(transform.position.x);
+        }
+
+        _audioManager.PlaySFX(_audioManager._jump);
+        _visuPlayer.RotationLeft();
+        _currentPositionX -= _movement;
+        ApplyXPosition();
+        _echo.InvokeEchoMoveLeft(transform.position);
+        if (_lastIcePlatform != null)
+        {
+            _lastIcePlatform.enabled = false;
+        }
+        transform.SetParent(null);
+        _echo.InvokeEchoStopSlide(transform.position);
+    }
+    public void MoveUp()
+    {
+        _audioManager.PlaySFX(_audioManager._jump);
+        _visuPlayer.RotationUp();
+        _currentPositionZ += _movement;
+        ApplyZPosition();
+        _echo.InvokeEchoMoveUp(transform.position);
+        transform.SetParent(null);
+        _echo.InvokeEchoStopSlide(transform.position);
+    }
+    public void MoveDown()
+    {
+        _audioManager.PlaySFX(_audioManager._jump);
+        _visuPlayer.RotationDown();
+        _currentPositionZ -= _movement;
+        ApplyZPosition();
+        _echo.InvokeEchoMoveDown(transform.position);
+        transform.SetParent(null);
+        _echo.InvokeEchoStopSlide(transform.position);
+    }
+    private void ApplyXPosition()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x = _currentPositionX;
+        _transform.position = newPosition;
+
+        _player.PlayMovementTween(newPosition);
+    }
+    private void ApplyZPosition()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z = _currentPositionZ;
+        _transform.position = newPosition;
+
+        _player.PlayMovementTween(newPosition);
+    }
+    public void SpawnGround()
+    {
+        _followPlayer.MoveUp();
+        _maxPositionZReach = _currentPositionZ;
+        _score++;
+        _groundSpawn.SpawnGround(transform.position);
     }
 }
